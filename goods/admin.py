be825625ba769032c635d09464a5a159dca3ded4
Kumyrkan <129401313+Kumyrkan@users.noreply.github.com>
2024-03-13 00:23:18 +0600

    3

--- goods/admin.py
+++ goods/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from goods.models import Good, Category, Review
 
 admin.site.register(Category)
-admin.site.register(Good)
 admin.site.register(Review)
 
 # Register your models here.
--- shop/settings.py
+++ shop/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'goods.apps.GoodsConfig',
     'orders.apps.OrdersConfig',
     'api.apps.ApiConfig',
+    'authorization.apps.AuthorizationConfig',
     'tinymce',
     'django_extensions',
 ]
@@ -122,7 +123,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+AUTH_USER_MODEL = 'authorization.User'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
